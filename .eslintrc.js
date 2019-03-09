62d54e78930837629b254a20890a712c0068f826
Emily Wenberg <martiansnoop@gmail.com>
2019-03-09 11:11:30 -0800

    Add sourceType:module to default eslintrc
    Basically everything I write is a module, or it doesn't matter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
         "es6": true,
         "node": true
     },
+    "sourceType": "module",
     "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
