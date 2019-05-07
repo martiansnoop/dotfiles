a80274aa949ba6da978d12279fd79b176b34b15f
Emily Wenberg <martiansnoop@gmail.com>
2019-05-06 22:26:10 -0700

    Update autokey

---
+++ .config/autokey/data/My Phrases/get_active_title.py
@@ -0,0 +1,5 @@
+import time 
+time.sleep(2)
+winTitle = window.get_active_title()
+winClass = window.get_active_class()
+dialog.info_dialog("Window information", "Active window information:\\nTitle: '%s'\\nClass: '%s'" % (winTitle, winClass))
