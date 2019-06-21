bc2550a9eb6cff865dff4a19eeb9121a40899bb5
Emily Wenberg <martiansnoop@gmail.com>
2019-06-21 14:21:36 -0700

    Add delay to some keystrokes to hopefully eliminate weird behaviour

--- .config/autokey/data/MacOS/save.py
+++ .config/autokey/data/MacOS/save.py
@@ -2,4 +2,5 @@
 if window.get_active_class() in ['Alacritty.Alacritty', 'gnome-terminal-server.Gnome-terminal']:
     keyboard.send_keys("<super>+s")
 else:
+    time.sleep(.1)
     keyboard.send_keys("<ctrl>+s")
