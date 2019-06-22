b2f5e9e030e88b6a89dd2158bf9c23061c73739e
Emily Wenberg <martiansnoop@gmail.com>
2019-06-22 10:02:43 -0700

    Don't show git prompt's brackets if not in a git prompt

--- .no-my-zsh/lib/git.zsh
+++ .no-my-zsh/lib/git.zsh
@@ -3,7 +3,7 @@ function git_prompt_info() {
   local ref
   ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
       ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-  echo "${ref#refs/heads/}$(parse_git_dirty)"
+  echo "[${ref#refs/heads/}$(parse_git_dirty)]"
 }
 
 # Checks if working tree is dirty
