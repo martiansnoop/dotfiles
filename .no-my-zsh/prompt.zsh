2f3609a5d12f8e84dcf93add6d2e3bffe0e11468
Emily Wenberg <martiansnoop@gmail.com>
2019-06-23 21:45:36 -0700

    Add error status to prompt if last command exited with nonzero

--- .no-my-zsh/prompt.zsh
+++ .no-my-zsh/prompt.zsh
@@ -2,6 +2,14 @@
 # as opposed to once, when this file is run.
 color='%B%F{black}'
 decolor='%b%f'
-PROMPT='$color%n@%m > $decolor'
+
+omg_color='%B%F{001}'
+
+PROMPT='$color%n@%m $omg_color$(error_status)$color> $decolor'
 RPROMPT='$color$(git_prompt_info) %3~$decolor'
+
+function error_status() {
+    last_command=$?
+    [[ last_command -ne 0 ]] && echo " [$last_command] "
+}
 
