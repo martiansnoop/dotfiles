5622868f42d6a399b7cec5c77c091f5ec2494636
Emily Wenberg <martiansnoop@gmail.com>
2019-06-24 21:55:15 -0700

    Delete more omz aliases
    omg omz ily but no plz how about we type whole commands

--- .no-my-zsh/lib/misc.zsh
+++ .no-my-zsh/lib/misc.zsh
@@ -18,16 +18,6 @@ fi
 ## jobs
 setopt long_list_jobs
 
-## super user alias
-alias _='sudo'
-
-## more intelligent acking for ubuntu users
-if which ack-grep &> /dev/null; then
-  alias afind='ack-grep -il'
-else
-  alias afind='ack -il'
-fi
-
 # only define LC_CTYPE if undefined
 if [[ -z "$LC_CTYPE" && -z "$LC_ALL" ]]; then
 	export LC_CTYPE=${LANG%%:*} # pick the first entry from LANG
