660e7ec583f386951138e9a0bac48fe52317c9a7
Emily Wenberg <martiansnoop@gmail.com>
2019-06-24 22:10:54 -0700

    Delete some version checks from omz code
    5.1 is from 2015 and I doubt I'll be working on a system where it's
    still in use.

--- .no-my-zsh/lib/misc.zsh
+++ .no-my-zsh/lib/misc.zsh
@@ -1,27 +1,10 @@
-autoload -Uz is-at-least
-
-# *-magic is known buggy in some versions; disable if so
-if [[ $DISABLE_MAGIC_FUNCTIONS != true ]]; then
-  for d in $fpath; do
-  	if [[ -e "$d/url-quote-magic" ]]; then
-  		if is-at-least 5.1; then
-  			autoload -Uz bracketed-paste-magic
-  			zle -N bracketed-paste bracketed-paste-magic
-  		fi
-  		autoload -Uz url-quote-magic
-  		zle -N self-insert url-quote-magic
-      break
-  	fi
-  done
-fi
+autoload -Uz bracketed-paste-magic
+zle -N bracketed-paste bracketed-paste-magic
+autoload -Uz url-quote-magic
+zle -N self-insert url-quote-magic
 
 ## jobs
 setopt long_list_jobs
-
-# only define LC_CTYPE if undefined
-if [[ -z "$LC_CTYPE" && -z "$LC_ALL" ]]; then
-	export LC_CTYPE=${LANG%%:*} # pick the first entry from LANG
-fi
 
 # recognize comments
 setopt interactivecomments
