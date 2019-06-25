2e69ea28268bc689dbac318cd9463941b1a4e32c
Emily Wenberg <martiansnoop@gmail.com>
2019-06-24 22:29:29 -0700

    Collect some disparate omz settings into one file
    This will let me look at them as a whole and maybe merge them back into
    my zshrc at some point.

--- .no-my-zsh/lib/misc.zsh
+++ .no-my-zsh/lib/misc.zsh
@@ -8,3 +8,26 @@ setopt long_list_jobs
 
 # recognize comments
 setopt interactivecomments
+
+function zsh_stats() {
+  fc -l 1 | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
+}
+
+# Required for $langinfo
+zmodload zsh/langinfo
+
+# Changing/making/removing directory
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+# ls colors
+autoload -U colors && colors
+
+# Enable ls colors
+export LSCOLORS="Gxfxcxdxbxegedabagacad"
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+setopt auto_cd
+setopt multios
+setopt prompt_subst
