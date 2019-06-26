450eaf52a4364f2b6aa8e14981fb3790c0f7a279
Emily Wenberg <martiansnoop@gmail.com>
2019-06-26 11:43:03 -0700

    Exclude vscode from close tab shortcut
    For some reason, autokey behaves weirdly with vscode in some contexts.

--- .config/autokey/data/MacOS/close tab2.py
+++ .config/autokey/data/MacOS/close tab2.py
@@ -1,5 +1,7 @@
 if window.get_active_class() in ['Alacritty.Alacritty', 'gnome-terminal-server.Gnome-terminal']:
     keyboard.send_keys("<ctrl>+b")
     keyboard.send_keys("x")
+elif window.get_active_class() in ['code.Code']:
+    keyboard.send_keys("<super>+w")
 else:
     keyboard.send_keys("<ctrl>+w")
