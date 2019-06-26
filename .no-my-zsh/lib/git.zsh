56a741fc9a594b86bdaff5d1a7ee0edca8c4a1a2
Emily Wenberg <martiansnoop@gmail.com>
2019-06-25 21:06:47 -0700

    Revert "Update prompt to use vcs-info for git prompt"
    This reverts commit 321b5bc612985bd091e9797acf3e62058585bd78.
    
    I'm not entirely sure this is actually better. I think in some cases my
    data was incorrect (I think ordering of precmd functions is part of it).

---
+++ .no-my-zsh/lib/git.zsh
@@ -0,0 +1,12 @@
+# Outputs current branch info in prompt format
+function git_prompt_info() {
+  local ref
+  ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+      ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+  echo "[${ref#refs/heads/}$(parse_git_dirty)]"
+}
+
+# Checks if working tree is dirty
+function parse_git_dirty() {
+  [[ -n  $(git status --porcelain --ignore-submodules=dirty) ]] && echo "*"
+}
