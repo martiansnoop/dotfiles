4589855bdcb1493ab7224d37f9bdade4c752bde2
Emily Wenberg <martiansnoop@gmail.com>
2019-07-06 21:59:45 -0700

    Update tab closing autokey script to avoid os-specific double send

--- .config/autokey/data/MacOS/close tab2.py
+++ .config/autokey/data/MacOS/close tab2.py
@@ -1,7 +1,15 @@
+import platform
+
 if window.get_active_class() in ['gnome-terminal-server.Gnome-terminal']:
     keyboard.send_keys("<ctrl>+b")
     keyboard.send_keys("x")
 elif window.get_active_class() in ['code.Code', 'Alacritty.Alacritty']:
-    keyboard.send_keys("<super>+w")
+    os, version, codename = platform.linux_distribution()
+    if os == 'Ubuntu':
+        # for some reason, need to passthrough manually here
+        # but on other linuxes, this sends a double key
+        keyboard.send_keys("<super>+w");
+    else:
+        pass
 else:
     keyboard.send_keys("<ctrl>+w")
